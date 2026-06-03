d19c46529db154d5dab4588ece2df8591215d43e
Garvit Jain <145357779+GARVITJAIN-1@users.noreply.github.com>
2026-06-03 15:22:49 +0530

    Update agent import from langchain_classic

--- app4.py
+++ app4.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from langchain_groq import ChatGroq
 from langchain_community.tools import DuckDuckGoSearchRun
-from langchain.agents import initialize_agent, AgentType
+from langchain_classic.agents import initialize_agent, AgentType
 
 st.set_page_config(page_title="LangChain Search Chatbot")
 st.title("LangChain - Chat with Search")
